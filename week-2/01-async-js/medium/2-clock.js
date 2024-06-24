0c36acc635645b3d81b7d24fc4a197ada682af1a
FaizanAanwar2801 <iamanwarfaizan@gmail.com>
2024-06-24 23:30:35 +0530

    Week-2 Clock in Console Assignment.

---
+++ week-2/01-async-js/medium/2-clock.js
@@ -0,0 +1,37 @@
+// Using `1-counter.md` or `2-counter.md` from the easy section, can you create a
+// clock that shows you the current machine time?
+
+// Can you make it so that it updates every second, and shows time in the following formats -
+
+// - HH:MM::SS (Eg. 13:45:23)
+
+// - HH:MM::SS AM/PM (Eg 01:45:23 PM)
+
+function showTime() {
+    var date = new Date();
+    var h = date.getHours(); // 0 - 23
+    var m = date.getMinutes(); // 0 - 59
+    var s = date.getSeconds(); // 0 - 59
+    var session = "AM";
+
+    if (h == 0) {
+        h = 12;
+    }
+
+    if (h > 12) {
+        h = h - 12;
+        session = "PM";
+    }
+
+    h = (h < 10) ? "0" + h : h;
+    m = (m < 10) ? "0" + m : m;
+    s = (s < 10) ? "0" + s : s;
+
+    var time = h + ":" + m + ":" + s + " " + session
+
+    setTimeout(showTime, 1000);
+    console.log(time) // logs time after every second
+
+}
+
+showTime();
