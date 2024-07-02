e2d7991298921189b9cea70b7dfd1cae1c3f687c
FaizanAanwar2801 <iamanwarfaizan@gmail.com>
2024-07-03 04:55:24 +0530

    Adding Week-6 useRef Assignments.

---
+++ week-6/3-use-ref/src/App.jsx
@@ -0,0 +1,20 @@
+import './App.css'
+// import { Assignment1 } from './components/Assignment1'
+// import { Assignment2 } from './components/Assignment2'
+
+function App() {
+  return (
+    <>
+      {/* <Assignment1 /> */}
+      {/* <Assignment2 /> */}
+    </>
+  )
+}
+
+// un-comment each import comment and return Assignment respectively
+// change directory to the source folder "1-use-memo"
+// type command "npm run dev"
+
+export default App
+
+
