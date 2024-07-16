768aa437762b42483ccda8f7abf97bcf82229939
FaizanAanwar2801 <iamanwarfaizan@gmail.com>
2024-07-16 17:31:22 +0530

    Week-7 Assignment-1 (modified template)

--- week-7/src/components/Assignment1.jsx
+++ week-7/src/components/Assignment1.jsx
@@ -1,1 +1,13 @@
-// Assignment1 template
+import '../index.css'
+
+const Assignment = () => {
+    return (
+        <div className='flex justify-center text-black align-middle'>
+            <b>
+            Assignment - 1
+            </b>
+        </div>
+    )
+}
+
+export default Assignment
