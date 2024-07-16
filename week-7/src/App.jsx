905c07163a737a42005adddf61455c94e98ef250
FaizanAanwar2801 <iamanwarfaizan@gmail.com>
2024-07-16 17:38:27 +0530

    Updeted-Files.

--- week-7/src/App.jsx
+++ week-7/src/App.jsx
@@ -1,20 +1,34 @@
 import './App.css'
+import './index.css'
 import Assignment1 from './components/Assignment1.jsx'
-import { Routes, Route } from 'react-router-dom'
+import Assignment2 from './components/Assignment2.jsx'
+import Landing from './components/Landing.jsx'
+import Navbar from './components/Navbar.jsx'
+import { Routes, Route, BrowserRouter } from 'react-router-dom'
 
 function App() {
 
 
   return (
-    <>
+    <div>
+      <BrowserRouter>
+      <Navbar/>
       <Routes>
-      <Route
-          path='/assignment1'
-          element={<Assignment1 />}
+        <Route 
+        path = '/' 
+        element = {<Landing/>}
+        />
+        <Route 
+        path = '/assignment1' 
+        element = {<Assignment1/>}
+        />
+        <Route 
+        path = '/assignment2' 
+        element = {<Assignment2/>}
         />
       </Routes>
-
-    </>
+      </BrowserRouter>
+    </div>
   )
 }
 
