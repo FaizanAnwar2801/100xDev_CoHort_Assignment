6ce472e246dc706a874a40b7c914f38a1b06b356
FaizanAanwar2801 <iamanwarfaizan@gmail.com>
2024-07-22 01:39:29 +0530

    Week-7 Assignment-2 (Completed)

--- week-7/src/components/Assignment2.jsx
+++ week-7/src/components/Assignment2.jsx
@@ -1,11 +1,65 @@
+import { useState } from 'react'
 import '../index.css'
 
-const Assignment = ()=>{
+const colors = [
+    {
+        name: 'Red',
+        value: '#f70505',
+    },
+    {
+        name: 'Yellow',
+        value: '#ffff00',
+    },
+    {
+        name: 'Black',
+        value: '#000000',
+    },
+    {
+        name: 'Purple',
+        value: '#a020f0',
+    },
+    {
+        name: 'Green',
+        value: '#2b771d',
+    },
+    {
+        name: 'Blue',
+        value: '#0000ff',
+    },
+
+    {
+        name: 'Default',
+        value: '#f76a05',
+    }
+    ]
+
+const Assignment2 = ()=>{
+const [color , setColor] = useState()
+
     return (
-        <div>
-            Assignment-2
+        <div style={{background:color}}>
+            <div className='flex flex-col justify-end w-screen h-screen'>
+                <div className='flex items-center justify-center py-2'>
+                    <div className='flex p-2 rounded-md space-x-2 bg-slate-200 '>
+                    {colors.map((value)=>{
+                        return(
+                            <button
+                            key = {value.value}
+                            onClick={()=>{
+                                setColor(value.value)
+                            }}
+                            style={{background:value.value, }}
+                            type='button'
+                            className='w-20 p-2 text-center text-white rounded-lg'>
+                                {value.name}
+                            </button>
+                        )
+                    })}
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
 
-export default Assignment
+export default Assignment2
