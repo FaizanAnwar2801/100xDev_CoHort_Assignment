a109841b1234a3a4e14a58bf992a3a1b959d2987
FaizanAanwar2801 <iamanwarfaizan@gmail.com>
2024-07-22 22:05:11 +0530

    Week-7 Assignment-3 (Completed)

---
+++ week-7/src/components/Assignment3.jsx
@@ -0,0 +1,42 @@
+/* eslint-disable react/prop-types */
+
+const Assignment3 = ()=>{
+    const anchorElement = {
+        type: 'a',
+        props: {
+        href: 'https://github.com/FaizanAnwar2801',
+        target: '_blank',
+        children: ['Click me'],
+        },
+    }
+
+    function generateHTML(element) {
+        const { type, props } = element
+        const children = props.children || []
+        const attributes = Object.keys(props)
+            .filter((key) => key !== 'children')
+            .map((key) => `${key}="${props[key]}"`)
+            .join(' ')
+    
+        const childHTML = children.map((child) => {
+            return typeof child === 'object' ? generateHTML(child) : child
+            }).join('')
+            return (`<${type} ${attributes}>${childHTML}</${type}>`)
+    }
+    
+    function customRender(element, container) {
+        const html = generateHTML(element)
+        const selector = document.querySelector(container)
+        selector.innerHTML = html
+    }
+
+    return(
+        <div className='flex items-center justify-center w-screen py-20'>
+            {customRender(anchorElement, '#root')}
+        </div>
+    )
+}
+
+
+
+export default Assignment3
