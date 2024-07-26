0ff8e73bf1404d34ecde10de4e1c556a6000205f
FaizanAanwar2801 <iamanwarfaizan@gmail.com>
2024-07-26 15:04:57 +0530

    Adding Nav Bar (Modified-bg inherit.)

--- week-7/src/components/Navbar.jsx
+++ week-7/src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom"
 const Navbar = ()=>{
     return(
         <div className='fixed top-0 z-5 '>
-            <div className='flex justify-between w-screen px-8 py-2 text-lg font-bold text-black bg-slate-100 h-max'>
+            <div className='flex justify-between w-screen px-8 py-2 text-lg font-bold text-black bg-inherit h-max'>
                 <Link to={'/'}>Week 7</Link>
                 <Link to={'/'}>Home</Link>
             </div>
