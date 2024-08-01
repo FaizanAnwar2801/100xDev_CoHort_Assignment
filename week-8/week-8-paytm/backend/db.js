6eb349b6cbffcde7fd8a74556e8e24c29322e98f
FaizanAanwar2801 <iamanwarfaizan@gmail.com>
2024-08-01 18:49:28 +0530

    Adding Database and mongoose Schema(Account schema added).

--- week-8/week-8-paytm/backend/db.js
+++ week-8/week-8-paytm/backend/db.js
@@ -27,10 +27,28 @@ const userSchema = mongoose.Schema({
         trim:true,
         maxLength:50,
     },
-})
+});
 
-const User = mongoose.model("User", userSchema);
+/************************************************************************************************************/
+
+const accountSchema = new mongoose.Schema({
+    userId: {
+        type: mongoose.Schema.Types.ObjectId, // Reference to User model
+        ref: 'User',
+        required: true
+    },
+    balance: {
+        type: Number,
+        required: true
+    }
+});
+
+/************************************************************************************************************/
+
+const Account = mongoose.model('Account', accountSchema);
+const User = mongoose.model('User', userSchema);
 
 module.exports = {
-    User
+    User,
+    Account,
 };
