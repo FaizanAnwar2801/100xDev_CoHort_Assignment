6fd8c77a633af6ffb29c871a59f86b027b5bdf0e
FaizanAanwar2801 <iamanwarfaizan@gmail.com>
2024-08-02 19:47:35 +0530

    fixed typos.

--- week-8/week-8-paytm/backend/routes/userRouter.js
+++ week-8/week-8-paytm/backend/routes/userRouter.js
@@ -3,7 +3,7 @@ const express = require("express");
 const zod = require("zod");
 const { User } = require("../db");
 const jwt = require("jsonwebtoken")
-const JWT_SECRET = require("../config");
+const {JWT_SECRET} = require("../config");
 const userRouter = express.Router();
 const  { authMiddleware } = require("../middleware");
 
@@ -64,21 +64,20 @@ userRouter.post("/signup", async (req, res) => {
 
 const signinSchema = zod.object({
     username: zod.string().email(),
-    password: zod.string()
+	password: zod.string()
 })
 
-userRouter.post("/signin", async (req, res) => {
-    const body = req.body;
-    const { success } = signinSchema.safeParse(body)
+router.post("/signin", async (req, res) => {
+    const { success } = signinSchema.safeParse(req.body)
     if (!success) {
         return res.status(411).json({
-            message: "Incorrect inputs"
+            message: "Email already taken / Incorrect inputs"
         })
     }
 
     const user = await User.findOne({
-        username: body.username,
-        password: body.password,
+        username: req.body.username,
+        password: req.body.password
     });
 
     if (user) {
@@ -86,16 +85,16 @@ userRouter.post("/signin", async (req, res) => {
             userId: user._id
         }, JWT_SECRET);
 
-        res.status(200).json({
+        res.json({
             token: token
         })
         return;
     }
 
+    
     res.status(411).json({
-        message: "Error while logging in."
+        message: "Error while logging in"
     })
-
 })
 
 /************************************************************************************************************/
