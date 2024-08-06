340483ad096289550d8bcafc08404d00ddbd075a
FaizanAanwar2801 <iamanwarfaizan@gmail.com>
2024-08-06 20:03:15 +0530

    Adding Balance Component(Display Balance)

--- week-8/week-8-paytm/frontend/src/components/Balance.jsx
+++ week-8/week-8-paytm/frontend/src/components/Balance.jsx
@@ -1,6 +1,23 @@
 /* eslint-disable react/prop-types */
 
-export const Balance = ({ value }) => {
+import axios from "axios"
+import { useEffect, useState } from "react"
+
+export const Balance = () => {
+
+    const [value, setValue] = useState(0)
+
+    useEffect(()=>{
+        axios.get("http://localhost:3000/api/v1/account/balance",{
+            headers: {
+                Authorization: "Bearer " + localStorage.getItem("token")
+            }
+        })
+        .then(response => {
+            setValue(Math.floor(response.data.balance))
+        })
+    })
+
     return <div className="flex">
         <div className="font-bold text-lg">
             Your balance
