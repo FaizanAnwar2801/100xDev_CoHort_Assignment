2e8b345a09861e6cbe8a50836cd3f962cbbe5cf5
FaizanAanwar2801 <iamanwarfaizan@gmail.com>
2024-08-08 19:44:02 +0530

    Front-end updated.

--- week-8/week-8-paytm/frontend/src/App.jsx
+++ week-8/week-8-paytm/frontend/src/App.jsx
@@ -1,9 +1,23 @@
-import './App.css'
+import { BrowserRouter, Routes , Route} from "react-router-dom";
+import { Signup } from "./pages/SignUp";
+import { Signin } from "./pages/Signin";
+import { Dashboard } from "./pages/Dashboard";
+import { SendMoney } from "./pages/SendMoney";
+import Landing from "./pages/Landing";
 
 function App() {
   return(
     <div>
-      "Front-end template."
+      <BrowserRouter>
+      <Routes>
+      <Route path="/" element={<Landing/>} />
+        <Route path="/signup" element={<Signup/>} />
+        <Route path="/login" element={<Signin/>}/>
+        <Route path="/dashboard" element={<Dashboard/>}/>
+        <Route path="/send" element={<SendMoney/>}/>
+        <Route/>
+      </Routes>
+      </BrowserRouter>
     </div>)
 }
 
