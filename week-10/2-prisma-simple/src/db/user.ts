a5dd7007c7a2ef2945385426f2e36bd7fd334dc0
FaizanAanwar2801 <iamanwarfaizan@gmail.com>
2024-08-14 19:28:53 +0530

    User Assignment(prisma)

---
+++ week-10/2-prisma-simple/src/db/user.ts
@@ -0,0 +1,42 @@
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+
+/*
+ * Should insert into the users table
+ * Should return the User object
+ * {
+ *   username: string,
+ *   password: string,
+ *   name: string
+ * }
+ */
+
+export async function createUser(username: string, password: string, name: string) {
+    const res = await prisma.user.create({
+        data:{
+            username,
+            password,
+            name,
+        }
+    })
+    return res;
+}
+
+/*
+ * Should return the User object
+ * {
+ *   username: string,
+ *   password: string,
+ *   name: string
+ * }
+ */
+export async function getUser(userId: number) {
+    const findUser = await prisma.user.findUnique({
+        where : {
+            id:userId,
+        }
+    })
+    return findUser;
+}
