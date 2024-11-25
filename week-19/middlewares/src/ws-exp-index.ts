f9e149945a3af29f139beedefc386522c9236a2a
FaizanAanwar2801 <iamanwarfaizan@gmail.com>
2024-11-25 13:25:17 +0530

    Websockets index-2.

---
+++ week-19/middlewares/src/ws-exp-index.ts
@@ -0,0 +1,29 @@
+import express from 'express'
+import { WebSocketServer , WebSocket} from 'ws'
+
+const app = express()
+
+app.get("/", (req,res)=>{
+    res.send("Hi There....")
+})
+
+const httpServer = app.listen(8080)
+
+const wss = new WebSocketServer({ server: httpServer });
+
+wss.on('connection', function connection(ws) {
+  ws.on('error', console.error);
+
+  ws.on('message', function message(data, isBinary) {
+
+    // console.log("recieved : %s", data) -- line to test recieved msgs on console log.
+
+    wss.clients.forEach(function each(client) {
+      if (client.readyState === WebSocket.OPEN) {
+        client.send(data, { binary: isBinary });
+      }
+    });
+  });
+
+  ws.send('Hello! Message From Server!!');
+});
