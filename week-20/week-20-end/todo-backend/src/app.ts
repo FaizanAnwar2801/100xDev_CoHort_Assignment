737da170689e0b1a0c20aec41d2f958a5136fcc4
FaizanAanwar2801 <iamanwarfaizan@gmail.com>
2024-12-06 02:05:20 +0530

    Backend App File(Updated for client.)

--- week-20/week-20-end/todo-backend/src/app.ts
+++ week-20/week-20-end/todo-backend/src/app.ts
@@ -1,6 +1,9 @@
 // src/app.ts
 import express, { json, urlencoded } from "express";
+import { Response as ExResponse, Request as ExRequest } from "express";
 import { RegisterRoutes } from "../build/routes";
+import swaggerUi from "swagger-ui-express";
+
 
 export const app = express();
 
@@ -13,3 +16,9 @@ app.use(
 app.use(json());
 
 RegisterRoutes(app);
+
+app.use("/docs", swaggerUi.serve, async (_req: ExRequest, res: ExResponse) => {
+  res.send(
+    swaggerUi.generateHTML(await import("../build/swagger.json"))
+  );
+});
